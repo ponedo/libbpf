0d82a149f506703d6a53e2140b501deb7cbb8c62
ponedo <2534164734@qq.com>
2021-11-25 11:07:24 +0000

    types.h modified

--- include/linux/types.h
+++ include/linux/types.h
@@ -20,6 +20,9 @@ typedef __u32 __bitwise __be32;
 typedef __u64 __bitwise __le64;
 typedef __u64 __bitwise __be64;
 
+typedef __u16 __bitwise __sum16;
+typedef __u32 __bitwise __wsum;
+
 #ifndef __aligned_u64
 # define __aligned_u64 __u64 __attribute__((aligned(8)))
 #endif
